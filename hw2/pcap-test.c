4007087f6e7e8eb08d9f7013cd6ac7357d4398d8
HohoHoui <wwsddrf15102@gmail.com>
2024-07-26 15:33:39 -0400

    homework pcap-test

---
+++ hw2/pcap-test.c
@@ -0,0 +1,113 @@
+#include <pcap.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "pcap-test.h"
+
+void usage() {
+	printf("syntax: pcap-test <interface>\n");
+	printf("sample: pcap-test wlan0\n");
+}
+
+typedef struct {
+	char* dev_;
+} Param;
+
+Param param = {
+	.dev_ = NULL
+};
+
+bool parse(Param* param, int argc, char* argv[]) {
+	if (argc != 2) {
+		usage();
+		return false;
+	}
+	param->dev_ = argv[1];
+	return true;
+}
+
+int main(int argc, char* argv[]) {
+	if (!parse(&param, argc, argv))
+		return -1;
+
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t* pcap = pcap_open_live(param.dev_, BUFSIZ, 1, 1000, errbuf);
+	if (pcap == NULL) {
+		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", param.dev_, errbuf);
+		return -1;
+	}
+
+	while (true) {
+		struct pcap_pkthdr* header;
+		const u_char* packet;
+		int res = pcap_next_ex(pcap, &header, &packet);
+		if (res == 0) continue;
+		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
+			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+			break;
+		}
+		printf("%u bytes captured\n", header->caplen);
+
+		struct Ethernet *Eth = (struct Ethernet *)packet;
+		struct IP *Ip = (struct IP*)(packet + sizeof(struct Ethernet));
+		struct TCP *Tcp = (struct TCP *)(packet + sizeof(struct Ethernet) + sizeof(struct IP));
+
+		printf("Ethernet des address : ");
+		for(int i = 0 ; i < MAC_ADDRESS_LEN; i++){
+			printf("%02x", Eth->des_add[i]);
+			if(i < MAC_ADDRESS_LEN - 1){
+				printf(":");
+			}
+		}
+
+		printf("\nEthernet src address : ");
+		for(int i = 0 ; i < MAC_ADDRESS_LEN ; i++){
+			printf("%02x", Eth->src_add[i]);
+			if(i < MAC_ADDRESS_LEN - 1){
+				printf(":");
+			}
+		}
+
+		printf("\nIP src address : ");
+		for(int i = 0 ; i < 4; i++){
+			printf("%02x", Ip -> src_add[i]);
+			if(i < 2){
+				printf(":");
+			}
+		}
+
+		printf("\nIP des address : ");
+		for(int i = 0 ; i < 4 ; i++){
+			printf("%02x", Ip -> des_add[i]);
+			if(i < 2){
+				printf(":");
+			}
+		}
+
+		printf("\nTCP src address : ");
+		for(int i = 0 ; i < 2; i++){
+			printf("%02x", Tcp->src_add[i]);
+			if(i == 0){
+				printf(":");
+			}
+		}
+
+		printf("\nTCP des address : ");
+		for(int i = 0 ; i < 2; i++){
+			printf("%02x", Tcp->des_add[i]);
+			if(i == 0){
+				printf(":");
+			}
+		}
+
+		printf("\nTCP PayLoad : ");
+		for(int i = 0 ; i < 4 ; i++){
+			printf("%02x", Tcp->payload[i]);
+			if(i < 3){
+				printf(":");
+			}
+		}
+		printf("\n");
+	}
+
+	pcap_close(pcap);
+}
---
+++ hw2/pcap-test.h
@@ -0,0 +1,31 @@
+#define MAC_ADDRESS_LEN 6
+typedef struct Ethernet{
+    unsigned char des_add[MAC_ADDRESS_LEN]; //need
+    unsigned char src_add[MAC_ADDRESS_LEN]; //need
+    unsigned char type[2];
+};
+
+//src_ip, dst_ip
+typedef struct IP{
+    unsigned char ver;
+    unsigned char dcsp;
+    unsigned char total_len[2];
+    unsigned char identification[2];
+    unsigned char fragment_offset[2];
+    unsigned char ttl[2];
+    unsigned char protocol;
+    //unsigned char header_checksum;
+    unsigned char src_add[4];
+    unsigned char des_add[4];
+};
+
+//src port,dst port
+typedef struct TCP{
+    unsigned char src_add[2];
+    unsigned char des_add[2];
+    unsigned char others[16];
+    unsigned char payload[4];
+};
+
+//payload(data) maximal : 20byte
+
